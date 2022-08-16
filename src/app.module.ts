6c529c9f992326b6e6f1ca1f5499050fcc58f157
shshank-a <shashank@cellpropulsion.com>
2022-08-16 11:14:39 +0530

    nestjs initital setup

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaService } from './providers/prisma.service';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
